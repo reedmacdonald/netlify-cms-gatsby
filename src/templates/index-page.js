3ea6a7c0e0d38bfe3c4b8c8f5fb4c682717f9ce2
Reed MacDonald <reedpmacdonald@gmail.com>
2020-11-25 09:31:21 -0800

    Some changes

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,10 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link, graphql } from "gatsby";
-
 import Layout from "../components/Layout";
 import Features from "../components/Features";
 import BlogRoll from "../components/BlogRoll";
+import idk from "./index.scss";
 
 export const IndexPageTemplate = ({
   image,
@@ -14,31 +14,14 @@ export const IndexPageTemplate = ({
   mainpitch,
   description,
   intro,
-}) => (
-  <div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content"></div>
-                <div className="columns"></div>
-
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest scholarships
-                  </h3>
-                  <BlogRoll />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </div>
-);
+}) => {
+  console.log(title, "<---title");
+  return (
+    <div style={{ width: "900px", margin: "0 auto" }}>
+      <h1>{title}</h1>
+    </div>
+  );
+};
 
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
