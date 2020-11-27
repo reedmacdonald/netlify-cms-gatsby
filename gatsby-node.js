b7f37fcea8f0f34952abf3f2718120f921641fe5
Reed MacDonald <reedpmacdonald@gmail.com>
2020-11-26 20:15:49 -0800

    Added color hopefully

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,6 +19,7 @@ exports.createPages = ({ actions, graphql }) => {
               tags
               templateKey
               alugAlternate
+              color
             }
           }
         }
