65219a4b939f0ade7f3bd7b814b1ae89cca1790e
Reed MacDonald <reedpmacdonald@gmail.com>
2020-11-30 19:27:30 -0800

    Deleted some stuff

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,6 +1,6 @@
 module.exports = [{
       plugin: require('../node_modules/gatsby-remark-images/gatsby-browser.js'),
-      options: {"plugins":[],"maxWidth":2048},
+      options: {"plugins":[],"maxWidth":2048,"linkImagesToOriginal":true,"showCaptions":false,"markdownCaptions":false,"sizeByPixelDensity":false,"backgroundColor":"white","quality":50,"withWebp":false,"tracedSVG":false,"loading":"lazy","disableBgImageOnAlpha":false,"disableBgImage":false},
     },{
       plugin: require('../node_modules/gatsby-plugin-netlify-cms/gatsby-browser.js'),
       options: {"plugins":[],"modulePath":"/Users/reedmacdonald/netlify-cms-gatsby/src/cms/cms.js"},
--- public/component---src-pages-404-js-1ddbc96043d38848727a.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("7oih");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement("div",null,l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-1ddbc96043d38848727a.js.map
